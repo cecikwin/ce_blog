ce6a3896dc47579b7ea9c2b6c1aa3b49d46edb48
张馨益（Cecilia）(zhangxinyi03) <zhangxinyi03@qiyi.com>
2022-01-05 16:13:57 +0800

    revert db

--- ceBlog/settings.py
+++ ceBlog/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'ceBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'root',
-#         'USER': 'root',
-#         'PASSWORD': '123456',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'root',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
 
 TIME_ZONE = 'Asia/Shanghai'
 
